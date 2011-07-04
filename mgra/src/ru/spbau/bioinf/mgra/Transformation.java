d28d4079945ddd5252d5260deb30216af20e311f
Yakov Sirotkin <yasha@telamon.ru>
2011-07-04 21:13:11 +0400

    Support reverse and fission. http://ablab.myjetbrains.com/youtrack/issue/MGRA-10

--- mgra/src/ru/spbau/bioinf/mgra/Transformation.java
+++ mgra/src/ru/spbau/bioinf/mgra/Transformation.java
@@ -3,6 +3,8 @@ package ru.spbau.bioinf.mgra;
 import org.jdom.Element;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Transformation {
@@ -36,6 +38,55 @@ public class Transformation {
             }
             before.addContent(chromosome.toXml());
         }
+        if (chromosomes.size() ==1) {
+            Chromosome chr = chromosomes.get(0);
+            List<Gene> genes = chr.getGenes();
+            LinkedList<Position> positions = new LinkedList<Position>();
+            for (int i = 0; i < genes.size(); i++) {
+                Gene gene = genes.get(i);
+                gene.addPositions(positions, i);
+            }
+            Collections.sort(positions);
+            Position first = positions.getFirst();
+            if (first.getPos() == 0 && first.getSide() == -1) {
+                positions.addFirst(new Position(-1,1, 0));
+            }
+            if (positions.size() > 2) {
+                reverseOrFision(all, genes, positions);
+                after.addContent(chr.toXml());
+            }
+            chr.clearEnds();
+        }
+    }
+
+    private void reverseOrFision(List<Chromosome> all, List<Gene> genes, LinkedList<Position> positions) {
+        Position left = positions.get(1);
+        Position right = positions.get(2);
+
+        int start = left.getPos();
+        int end = right.getPos();
+        if (left.getColor() % 2 != right.getColor() %2) {
+            List<Gene> reverse = new ArrayList<Gene>();
+            for (int pos = end; pos >=start; pos--) {
+                Gene gene = genes.get(pos);
+                gene.reverse();
+                reverse.add(gene);
+                genes.remove(pos);
+            }
+            for (int i = 0; i < reverse.size(); i++) {
+                genes.add(start + i, reverse.get(i));
+            }
+        } else {
+            List<Gene> cut = new ArrayList<Gene>();
+            for (int pos = start; pos <=end; pos++) {
+                cut.add(genes.get(start));
+                genes.remove(start);
+            }
+            Chromosome add = new Chromosome(all.size() + 1, cut);
+            all.add(add);
+            after.addContent(add.toXml());
+            add.clearEnds();
+        }
     }
 
     public Element toXml() {
@@ -47,4 +98,5 @@ public class Transformation {
         tr.addContent(after);
         return tr;
     }
+
 }
