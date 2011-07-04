ccc9921560bc117fca440b433a207a3c44c4af99
Yakov Sirotkin <yasha@telamon.ru>
2011-07-04 21:10:38 +0400

    Add reverse and getSide operations. http://ablab.myjetbrains.com/youtrack/issue/MGRA-10

--- mgra/src/ru/spbau/bioinf/mgra/Direction.java
+++ mgra/src/ru/spbau/bioinf/mgra/Direction.java
@@ -2,7 +2,27 @@ package ru.spbau.bioinf.mgra;
 
 
 public enum Direction {
-    MINUS("minus"), PLUS("plus");
+    MINUS("minus") {
+        @Override
+        int getSide(EndType endType) {
+            return endType == EndType.HEAD ? - 1: 1;
+        }
+
+        @Override
+        Direction reverse() {
+            return Direction.PLUS;
+        }
+    }, PLUS("plus") {
+        @Override
+        int getSide(EndType endType) {
+            return endType == EndType.TAIL ? - 1: 1;
+        }
+
+        @Override
+        Direction reverse() {
+            return Direction.MINUS;
+        }
+    };
 
     private String text;
 
@@ -13,6 +33,9 @@ public enum Direction {
     public static Direction getDirection(char ch) {
         return ch == '-' ? MINUS : PLUS;
     }
+
+    abstract int getSide(EndType endType);
+    abstract Direction reverse();
 
     @Override
     public String toString() {
